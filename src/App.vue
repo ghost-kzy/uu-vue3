3407109f56b0648c7268c414a16e25993a9bf58f
uu-kong <1056568558@qq.com>
2025-08-30 11:46:57 +0800

    uukong的vue3

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>{{ msg }}</h3>
+  <div v-html="html"></div>
+  <div>{{html}}</div>
+</template>
+<script setup>
+import { ref } from "vue";
+
+const msg = ref('孔中由')
+const html = `<h2>我是一段HTML代码</h2>`
+
+</script>
+
+<style scoped></style>
