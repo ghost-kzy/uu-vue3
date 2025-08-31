92f5f7ce8e059d026722b0e53cb8ea611a1ac1de
uu-kong <1056568558@qq.com>
2025-08-31 14:56:17 +0800

    完成基本布局

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
-  <h3>{{ msg }}</h3>
-  <div v-html="html"></div>
-  <div>{{html}}</div>
+  <TeamTest />
 </template>
 <script setup>
-import { ref } from "vue";
-
-const msg = ref('孔中由')
-const html = `<h2>我是一段HTML代码</h2>`
+import TeamTest from './components/Team-test.vue';
 
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
