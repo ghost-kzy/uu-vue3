791586423d2ed93c94dc77ffc6062f01eaf7a08d
uu-kong <1056568558@qq.com>
2025-09-01 22:39:26 +0800

    进一步拆分选项卡组件

---
+++ src/components/ItemHotBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="hot-bar">
+        <div class="inner">{{ hot }}热度</div>
+    </div>
+
+</template>
+
+<script setup>
+import { computed } from 'vue'
+const { hot, maxHot } = defineProps({
+    hot: {
+        type: Number,
+        default: 0
+    },
+    maxHot: {
+        type: Number,
+        default: 0
+    }
+})
+const HotWidth = computed(() => {
+    // return `${hot / maxHot}*100` + '%'
+    return `${hot / maxHot * 100}%`
+})
+</script>
+
+<style scoped>
+.hot-bar {
+    background-color: rgb(3, 37, 103);
+    border-radius: 20px;
+    text-indent: 0.5em;
+}
+
+.hot-bar .inner {
+    background-color: darkred;
+    border-radius: 20px;
+    width: v-bind(HotWidth);
+    /*进度条颜色渐变*/
+    background-image: linear-gradient(to right, rgb(187, 3, 52) 50%, rgb(66, 2, 12));
+    white-space: nowrap;
+}
+</style>
